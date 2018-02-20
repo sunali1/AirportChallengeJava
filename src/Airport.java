6f67e26393743b53e520a37b2b07bc7220e06ed7
Oleg Grushetskyy <olegfkl@gmail.com>
2018-02-20 16:42:45 +0000

    Created land method and pass test

--- src/Airport.java
+++ src/Airport.java
@@ -1,3 +1,8 @@
-public class Airport {
+import java.util.ArrayList;
 
+public class Airport {
+    ArrayList<String> hanger = new ArrayList<String>();
+    public Boolean land(String plane) {
+        return hanger.add(plane);
+    }
 }
--- test/AirportTest.java
+++ test/AirportTest.java
@@ -6,11 +6,16 @@ public class AirportTest {
 
 
     @Test
-    public void test() {
+    public void createInstance() {
         Airport heathrow = new Airport();
         assertTrue(heathrow instanceof Airport);
 
     }
 
+    @Test
+    public void land() {
+        Airport heathrow = new Airport();
+        assertEquals(true , heathrow.land("easyJet"));
 
+    }
 }
