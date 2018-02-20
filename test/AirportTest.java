f0dacadef9aacfc64a848d2e018508f41e1dee7f
sunaliseth <sunaliseth01@gmail.com>
2018-02-20 15:07:38 +0000

    Class Airport created and initialized test passes

--- test/AirportTest.java
+++ test/AirportTest.java
@@ -1,5 +1,16 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AirportTest {
+
+
+    @Test
+    public void test() {
+        Airport heathrow = new Airport();
+        assertTrue(heathrow instanceof Airport);
+
+    }
+
 
 }
